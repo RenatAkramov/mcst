6979753d5398d262278ef78099032d061de7975e
RenatAkramov <akramovrenat33@gmail.com>
2025-04-01 19:08:17 +0300

    task 9 first version of the algorithm
    first version of the algorithm and new comment

--- task9/sub.c
+++ task9/sub.c
@@ -26,6 +26,17 @@ int main(int argc, char* argv[])
     {
         printf("%d\n",numbers[i]);
     }
+
+    int start = 0;  // Начало максимальной последовательности
+    int end   = 0;  // Конец максимальной последовательности
+
+    find_max_sequence(numbers, count, &start, &end);
+
+    for (int i = start; i < end + 1; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+    return 1;
 }
 
 int* read_array(FILE* file, int* count)
@@ -47,4 +58,46 @@ int* read_array(FILE* file, int* count)
         numbers[(*count)++] = num;
     }    
     return numbers;
+}
+
+void find_max_sequence(int* arr, int count, int *start, int *end) 
+{
+    if (count == 0) // Проверка ситуации если массив пустой
+    {
+        *start = 0;
+        *end = -1; 
+        return;
+    }
+
+    int max_sum = arr[0];     // Максимальная сумма
+    int current_sum = arr[0]; // Текущая сумма
+    int current_start = 0;    // Начало текущей последовательности
+
+    for (int i = 1; i < count; ++i) 
+    {
+        if (arr[i] > arr[i - 1]) 
+        {
+            current_sum += arr[i];
+        } 
+        else 
+        {
+            // Проверяем, является ли текущая последовательность максимальной
+            if (current_sum > max_sum) 
+            {
+                max_sum = current_sum;
+                *start = current_start;
+                *end = i - 1;
+            }
+            // Начинаем новую последовательность с текущего элемента
+            current_start = i;
+            current_sum = arr[i];
+        }
+    }
+
+    // Проверяем последнюю последовательность
+    if (current_sum > max_sum) 
+    {
+        *start = current_start;
+        *end = count - 1;
+    }
 }
--- task9/sub.h
+++ task9/sub.h
@@ -4,3 +4,4 @@
 #include <stdlib.h>
 
 int* read_array(FILE* file, int* count);
+void find_max_sequence(int arr[], int count, int *start, int *end);
